e9739126260fa034933c660ed066010f1ec174e6
Ugur Tigu <ugurtigu@gmail.com>
2021-07-25 15:51:41 +0200

    creates user model class

---
+++ client/src/models/user.js
@@ -0,0 +1,9 @@
+export default class User {
+  constructor(username, password, github, twitter, email) {
+    this.username = username
+    this.password = password
+    this.github = github
+    this.twitter = twitter
+    this.email = email
+  }
+}
