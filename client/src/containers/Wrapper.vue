53d3016cf59e1096e3523c288bba0357bb321d04
Ugur Tigu <ugurtigu@gmail.com>
2021-07-25 15:50:01 +0200

    adds wrapper container for sizing components

---
+++ client/src/containers/Wrapper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Wrapper'
+})
+</script>
+
+<style>
+.wrapper {
+  padding-left: 600px;
+  padding-right: 600px;
+  padding-top: 100px;
+}
+</style>
